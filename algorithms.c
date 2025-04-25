a7002b04c70337ca6f33135676725052e3c08ff4
matteos24 <54120000+matteos24@users.noreply.github.com>
2025-04-25 16:09:15 -0400

    Update algorithms.c

--- algorithms.c
+++ algorithms.c
@@ -12,8 +12,12 @@ int64_t karmakar_karp(int64_t *arr, int n);
 int64_t repeated_random(int64_t *arr, int n, int max_iter);
 int64_t hill_climbing(int64_t *arr, int n, int max_iter);
 int64_t simulated_annealing(int64_t *arr, int n, int max_iter);
+int64_t pre_partition_repeated_random(int64_t *arr, int n, int max_iter);
+int64_t pre_partition_hill_climbing(int64_t *arr, int n, int max_iter);
+int64_t pre_partition_simulated_annealing(int64_t *arr, int n, int max_iter);
 
 int64_t dot_product(int64_t *arr1, int64_t *arr2, int n);
+int64_t* conversion(int64_t *arr1, int64_t *arr2, int n);
 double t_iter(int iter);
 double probability(int64_t residue1, int64_t residue2, int iter);
 
@@ -23,16 +27,12 @@ int main(void)
 {   
     srand(time(NULL));
     int n;
-    int64_t *arr = malloc(n * sizeof(*arr));
-    int64_t residue = karmarkar_karp(arr, n);
-    printf("The residue is %i", residue);
-    free(arr);
+    printf("%i", RAND_MAX);
 
 }
 
 int64_t karmarkar_karp(int64_t *arr, int n) 
 {
-    // First, we build a max-heap of the n elements
     MaxHeap *heap = createHeap(n);
     for (int i = 0; i < n; i++) {
         insert(heap, arr[i]);
@@ -178,8 +178,8 @@ int64_t simulated_annealing(int64_t *arr, int n, int max_iter)
 
     for (int i = 0; i < max_iter; i++)
     {
-        int index1 = rand() % (n - 1) + 1;
-        int index2 = rand() % (n - 1) + 1;
+        int index1 = rand() % (n);
+        int index2 = rand() % (n);
 
         sol1[index1] = -sol[index1];
         if ((float)rand() / (float)RAND_MAX >= 0.5)
@@ -196,14 +196,15 @@ int64_t simulated_annealing(int64_t *arr, int n, int max_iter)
         }
         else if((float)rand() / (float)RAND_MAX <= probability(residue, test_residue, i))
         {
+            residue = test_residue;
             sol[index1] = sol1[index1];
             sol[index2] = sol1[index2];
         }
 
-        test_residue_new = abs(dot_product(arr, sol, n));
+        residue = abs(dot_product(arr, sol, n));
         test_residue_new2 = abs(dot_product(arr, sol2, n));
 
-        if (test_residue_new < test_residue_new2)
+        if (residue < test_residue_new2)
         {
             sol2[index1] = sol[index1];
             sol2[index2] = sol[index2];
@@ -213,10 +214,166 @@ int64_t simulated_annealing(int64_t *arr, int n, int max_iter)
     }
 
     test_residue_new2 = abs(dot_product(arr, sol2, n));
+    free(sol);
+    free(sol1);
+    free(sol2);
     return test_residue_new2;
 
 }
 
+int64_t pre_partition_repeated_random(int64_t *arr, int n, int max_iter)
+{
+    int* sol = malloc(n * sizeof(int));
+    for (int j = 0; j < n; j++)
+    {
+        sol[j] = (rand() % (n));
+    }
+    int64_t* converted_array = conversion(sol, arr, n);
+    int64_t residue = karmarkar_karp(converted_array, n);
+    int64_t test_residue;
+
+    for (int i = 0; i < max_iter; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            sol[j] = (rand() % (n));
+        }
+        converted_array = conversion(sol, arr, n);
+        test_residue = karmarkar_karp(converted_array, n);
+
+        if (test_residue < residue)
+        {
+            residue = test_residue;
+        }
+    }
+
+    free(sol);
+    free(converted_array);
+    return residue;
+}
+
+
+int64_t pre_partition_hill_climbing(int64_t *arr, int n, int max_iter)
+{
+    int* sol = malloc(n * sizeof(int));
+    int* sol1 = malloc(n * sizeof(int));
+    int index1;
+    int index2;
+
+    for (int j = 0; j < n; j++)
+    {
+        sol[j] = (rand() % (n));
+        sol1[j] = sol[j];
+    }
+
+    int64_t* converted_array = conversion(sol, arr, n);
+    int64_t residue = karmarkar_karp(converted_array, n);
+    int64_t test_residue;
+    int64_t* second_converted_array = malloc(n* sizeof(int64_t));
+
+
+    for (int i = 0; i < max_iter; i++)
+    {
+        index1 = (rand() % (n));
+        index2 = (rand() % (n));
+
+        while(sol[index1] == index2)
+        {
+            index1 = (rand() % (n));
+            index2 = (rand() % (n));
+        }
+
+        sol1[index1] = index2;
+        second_converted_array = conversion(sol1, arr, n);
+        test_residue = karmarkar_karp(second_converted_array, n);
+
+        if (test_residue < residue)
+        {
+            residue = test_residue;
+            sol[index1] = sol1[index1];
+        }
+
+    }
+
+    free(sol);
+    free(sol1);
+    free(converted_array);
+    free(second_converted_array);
+    return residue; 
+}
+
+
+int64_t pre_partition_simulated_annealing(int64_t *arr, int n, int max_iter)
+{
+    int* sol = malloc(n * sizeof(int));
+    int* sol1 = malloc(n * sizeof(int));
+    int* sol2 = malloc(n * sizeof(int));
+    int index1;
+    int index2;
+
+    for (int j = 0; j < n; j++)
+    {
+        sol[j] = (rand() % (n));
+        sol1[j] = sol[j];
+        sol2[j] = sol[j];
+    }
+
+    int64_t* converted_array = conversion(sol, arr, n);
+    int64_t residue = karmarkar_karp(converted_array, n);
+    int64_t test_residue;
+    int64_t test_residue2;
+    int64_t* second_converted_array = malloc(n* sizeof(int64_t));
+    int64_t* third_converted_array = malloc(n* sizeof(int64_t));
+
+    for (int i = 0; i < max_iter; i++)
+    {
+        index1 = (rand() % (n));
+        index2 = (rand() % (n));
+
+        while(sol[index1] == index2)
+        {
+            index1 = (rand() % (n));
+            index2 = (rand() % (n));
+        }
+
+        sol1[index1] = index2;
+        second_converted_array = conversion(sol1, arr, n);
+        test_residue = karmarkar_karp(second_converted_array, n);
+        if (test_residue < residue)
+        {
+            residue = test_residue;
+            sol[index1] = sol1[index1];
+        }
+        else if ((float)rand() / (float)RAND_MAX <= probability(residue, test_residue, i))
+        {
+            residue = test_residue;
+            sol[index1] = sol1[index1];
+        }
+
+        converted_array = conversion(sol, arr, n);
+        residue = karmarkar_karp(converted_array, n);
+        third_converted_array = conversion(sol2, arr, n);
+        test_residue2 = karmakar_karp(third_converted_array, n);
+
+        if (residue < test_residue2)
+        {
+            sol2[index1] = sol[index1];
+        }
+
+    }
+
+    third_converted_array = conversion(sol2, arr, n);
+    test_residue2 = karmakar_karp(third_converted_array, n);
+    free(sol);
+    free(sol1);
+    free(sol2);
+    free(converted_array);
+    free(second_converted_array);
+    free(third_converted_array);
+
+    return test_residue2;
+
+}
 
 int64_t dot_product(int64_t *arr1, int64_t *arr2, int n)
 {
@@ -226,6 +383,21 @@ int64_t dot_product(int64_t *arr1, int64_t *arr2, int n)
         result  = result + (arr1[i] * arr2[i]);
     }
     return result;
+}
+
+int64_t *conversion(int64_t *arr1, int64_t *arr2, int n)
+{
+    int64_t* final_array = malloc(n* sizeof(int64_t));
+    for (int j  = 0; j < n; j++)
+    {
+        final_array[j] = 0;
+    }
+
+    for (int j  = 0; j < n; j++)
+    {
+        final_array[arr1[j]] =  final_array[arr1[j]] + arr2[j];
+    }
+    return final_array;
 }
 
 double t_iter(int iter)
